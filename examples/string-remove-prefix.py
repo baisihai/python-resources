2530a26dacffae0a3664152106e7c40ea91f44d9
baisihai <kilfer@hotmail.com>
2025-08-19 18:26:01 +0800

    new file:   examples/string-remove-prefix.py

---
+++ examples/string-remove-prefix.py
@@ -0,0 +1,11 @@
+links=[
+    "www.b001.com",
+    "www.yourdomain.com",
+    "www.example.com",
+    "www.testsite.com",
+    "www.wikipedia.com",
+    "abc.com"
+    ]
+
+for link in links:
+    print (link.removeprefix("www."))

492983198e57449ce0e0d48a4603d2bc7b51861a
baisihai <kilfer@hotmail.com>
2025-08-22 12:23:24 +0800

    new file:   examples/list-extend.py

---
+++ examples/list-extend.py
@@ -0,0 +1,16 @@
+inventory = [
+    'Sword',
+    'Armor',
+    'Boots'
+]
+
+chest = [
+    'Map',
+    'Potion', 
+    'Shield'
+]
+
+# Method: Using the extend() method
+print("Method: Using the extend() method")
+inventory.extend(chest)
+print("Updated Inventory:", inventory)

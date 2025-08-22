b6d48d419c44cee20d550d498321371b972afddc
baisihai <kilfer@hotmail.com>
2025-08-22 12:23:08 +0800

    modified:   examples/dictionary-aggregation.py

--- examples/dictionary-aggregation.py
+++ examples/dictionary-aggregation.py
@@ -2,24 +2,24 @@
 # Combines two dictionaries by summing the values of common keys.
 # Example: Aggregating two dictionaries representing an inventory and loot
 
-inv = {
+inventory = {
     'Sword': 1,
-    'Portion': 3
+    'Potion': 3
 }
 
 loot = {
     'Sword': 1,
-    'Portion': 2, 
+    'Potion': 2, 
     'Shield': 1
 }
 
 # Method 1: Using dictionary comprehension
 print("Method 1: Using dictionary comprehension")
-new_inv = {k: inv.get(k, 0) + loot.get(k, 0) for k in set(inv) | set(loot)}
-print ("Updated Inventory:", new_inv,"\n")
+new_inventory = {k: inventory.get(k, 0) + loot.get(k, 0) for k in set(inventory) | set(loot)}
+print ("Updated Inventory:", new_inventory,"\n")
 
 # Method 2: Using the update() method
 print("Method 2: Using the update() method")
-print("Note: The update() method only combines the keys with the values from loot into inv.")
-inv.update(loot)
-print ("Updated Inventory:", inv)
+print("Note: The update() method only combines the keys with the values from loot into inventory.")
+inventory.update(loot)
+print ("Updated Inventory:", inventory)

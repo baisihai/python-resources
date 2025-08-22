8fe9ed178d9977cdbd2954a1dbf2fe910e4df2b0
baisihai <kilfer@hotmail.com>
2025-08-22 12:23:43 +0800

    new file:   examples/list-plus.py

---
+++ examples/list-plus.py
@@ -0,0 +1,16 @@
+inventory = [
+    'Sword',
+    'Armor',
+    'Boots'
+]
+
+chest = [
+    'Map',
+    'Potion', 
+    'Shield'
+]
+
+# Method: Using the + operator
+print("Method: Using the + operator")
+new_inventory = inventory + chest
+print("Updated Inventory:", new_inventory)
